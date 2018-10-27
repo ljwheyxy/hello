861279cf104a9439ec106d66c39fae3b9412885a
linjiawei <linjiawei166@gmail.com>
2018-10-27 17:23:22 +0800

    Add files via upload

---
+++ pages/index/index.vue
@@ -0,0 +1,83 @@
+<template>
+	<view class="content">
+		<view class="uni-list">
+			<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,index) in news" :key="index" @tap="openinfo" :data-newsid="item.post_id">
+				<view class="uni-media-list">
+					<image class="uni-media-list-logo" :src="item.author_avatar"></image>
+					<view class="uni-media-list-body">
+						<view class="uni-media-list-text-top">{{item.title}}</view>
+						<view class="uni-media-list-text-bottom uni-ellipsis">{{item.created_at}}</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				news: []
+			}
+
+		},
+		onLoad: function() {
+			uni.showLoading({
+				title: '加载中...',
+				mask: false
+			});
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news',
+				method: 'GET',
+				data: {},
+				success: res => {
+					// console.log(res);
+					this.news = res.data;
+					uni.hideLoading();
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+		},
+		methods: {
+			openinfo(e) {
+				var newsid = e.currentTarget.dataset.newsid;
+				// console.log(newsid);
+				uni.navigateTo({
+					url: '../info/info?newsid='+newsid,
+					success: res => {},
+					fail: () => {},
+					complete: () => {}
+				});
+			}
+		},
+
+	}
+</script>
+
+<style>
+	.content {
+		flex: 1;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+
+	.uni-media-list-body {
+		height: auto;
+	}
+
+	.uni-media-list-logo {
+		height: 120upx;
+		width: 120upx;
+	}
+
+	.uni-media-list-text-top {
+		line-height: 1.6em;
+	}
+</style>
---
+++ pages/info/info.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="content">
+		<view class="title">{{title}}</view>
+		<view class="art-content">
+			<rich-text class="richText" :nodes="strings"><!-- {{strings}} --></rich-text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '',
+				strings : '',
+			}
+		},
+		onLoad:function(e){
+			uni.showLoading({
+				title: '加载中...',
+				mask: false
+			});
+			console.log(e);
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news/36kr/'+e.newsid,
+				method: 'GET',
+				data: {},
+				success: res => {
+					uni.hideLoading();
+					console.log(res.data);
+					this.title = res.data.title;
+					this.strings = res.data.content;//new String(res.data.content);
+					// this.strings.replace("\r\n","");//("\r\n","",this.strings);
+					console.log(this.strings);
+					
+				},
+				fail: () => {
+					this.strings = "加载失败！"
+				},
+				complete: () => {}
+			});
+		}
+	}
+</script>
+
+<style>
+.content{
+	padding:10upx 2%;
+	width: 96%;
+	flex-wrap:wrap;
+}
+.title{line-height:2em;font-weight:700;font-size:38upx}
+.art-content{line-height:2em}
+</style>
